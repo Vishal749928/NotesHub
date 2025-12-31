e130af17789e93414491e8db7f6c0def154cb71a
Vishal749928 <vishalsalunke7499@gmail.com>
2025-12-31 20:59:36 +0530

    fix port fallback for render production

--- server.js
+++ server.js
@@ -33,9 +33,8 @@ process.on("uncaughtException", err => {
 });
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("🚀 Server running on port", PORT);
 });
-
