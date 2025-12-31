2a5309ad573c3437c82b1e52b73a028a3428c798
Vishal749928 <vishalsalunke7499@gmail.com>
2025-12-31 19:42:07 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ process.on("uncaughtException", err => {
   console.error("Uncaught:", err);
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
+
